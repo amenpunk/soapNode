87dde368b23272714e8cae375e237694a42c5bf2
amenpunk <ondasycircuitos@gmail.com>
2022-02-03 11:46:14 -0600

    service name -> bind -> process

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const wsdl = require('fs').readFileSync('myservice.wsdl', 'utf8');
-const myServices = require('./myServices')
+const { myService } = require('./myServices');
 
-//express server example
 var app = express();
-//body parser middleware are supported (optional)
+
 app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
 app.listen(8080, function(){
-    //Note: /wsdl route will be handled by soap module
-    //and all other routes & middleware will continue to work
-    soap.listen(app, '/wsdl', myServices, wsdl , function(){
+
+    let server = soap.listen(app, '/wsdl', myService ,wsdl , function(){
         console.log('server initialized on port -> ', 8080  );
-        // console.log(Object.keys(app))
     });
 
-    soap.log = function (type, data) {
+    server.log = function (type, data) {
         console.log('SOAP >>', type, data)
     }
 
+    server.on('headers', function(headers, methodName) {
+        console.log(header, (methodName))
+    });
 
 });
 
 
+// /home/ming/WORKSPACE/SOPA_EXAMPLE/myservice.wsdl
--- myServices.js
+++ myServices.js
@@ -1,44 +1,12 @@
-module.exports.myService = {
-    MyService: {
-        MyPort: {
-            MyFunction: function(args) {
-                return {
-                    name: args.name
-                };
+let myService = {
+    MyServices: { // service name : 43
+        MyPort: { // port binding : 46
+            sayHello : function(args){ // operation
+                console.log(args)
+                return '1';
             },
-
-            // This is how to define an asynchronous function with a callback.
-            MyAsyncFunction: function(args, callback) {
-                // do some work
-                callback({
-                    name: args.name
-                });
-            },
-
-            // This is how to define an asynchronous function with a Promise.
-            MyPromiseFunction: function(args) {
-                return new Promise((resolve) => {
-                    // do some work
-                    resolve({
-                        name: args.name
-                    });
-                });
-            },
-
-            // This is how to receive incoming headers
-            HeadersAwareFunction: function(args, cb, headers) {
-                return {
-                    name: headers.Token
-                };
-            },
-
-            // You can also inspect the original `req`
-            reallyDetailedFunction: function(args, cb, headers, req) {
-                console.log('SOAP `reallyDetailedFunction` request from ' + req.connection.remoteAddress);
-                return {
-                    name: headers.Token
-                };
-            }
         }
     }
 }
+
+module.exports = { myService }
